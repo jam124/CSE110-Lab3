b4df57d3932d99416d2b3756024b5bec5f932e1a
jam124 <jam124@ucsd.edu>
2020-01-21 23:04:25 -0800

    more changes

--- app/src/androidTest/java/edu/ucsd/cse110/testproject/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/edu/ucsd/cse110/testproject/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package edu.ucsd.cse110.testproject;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
